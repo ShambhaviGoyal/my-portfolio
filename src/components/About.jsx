bead1ea703230a9cfcd7330e8a3820475bebc97e
Shambhavi Goyal <sgoyal3@buffalo.edu>
2025-04-06 16:21:38 -0400

    finished 'about' section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,11 @@
 const aboutItems = [
     {
       label: 'Project done',
-      number: 45
+      number: 10
     },
     {
       label: 'Years of experience',
-      number: 10
+      number: 5
     }
 ];
 
@@ -14,22 +14,26 @@ const About = () => {
         <section id="about" className="section">
 
         <div className="container">
-            <div className="">
-                <p className="">
-                Welcome! I&apos;m Henry, a professional web developer with a knack for crafting visually stunning and highly functional websites. Combining creativity and technical expertise. I transform your vision into digital masterpiece that excels in both appearance and performance.
+            <div className="bg-zinc-800/50 p-7 rounded-2xl md:p-12">
+                <p className="text-zinc-300 mb-4 md:mb-8 md:text-xl md:max-w-[60ch]">
+                Welcome! I&apos;m Shambhavi, a passionate full stack developer who brings ideas to life through clean code and thoughtful design. From building intuitive user interfaces to developing powerful backend systems, I specialize in creating end-to-end web solutions that are scalable, secure, and beautifully crafted. Let’s turn your vision into a seamless digital experience.
                 </p>
 
-                <div className="">
+                <div className="flex flex-wrap items-center gap-4 md:gap-7">
                     {
-                        aboutItems.map(({labe, number}, key) => (
+                        aboutItems.map(({label, number}, key) => (
                             <div key={key}>
-                                <div className="">
-                                    <span className="">{number}</span>
-                                    <span className="">+</span>
-                                    </div>
+                                <div className="flex items-center md:mb-2">
+                                    <span className="text-2xl font-semibold md:text-4xl">{number}</span>
+                                    <span className="text-sky-400 font-semibold md:text-3xl">+</span>
+                                </div>
+                                <p className="text-sm text-zinc-400">{label}</p>  
+
                             </div>
                         ))
                     }
+
+                    <img src="/images/logo.svg" alt="logo" width={30} height={30} className="ml-auto md:w-[40px] md:h-[40px]" />
 
                 </div>
             </div>
