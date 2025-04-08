22c575059a36599115290d2e624971fbbeaae1fe
Shambhavi Goyal <sgoyal3@buffalo.edu>
2025-04-07 20:33:15 -0400

    finished the Contact section

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,23 @@ import About from './components/About';
 import Skill from './components/Skill';
 import Education from './components/Education';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 const App = () => {
   return(
     <>
     <Header />
-    <Hero />
-    <About />
-    <Skill />
-    <Education />
-    <Projects />
+    <main>
+      <Hero />
+      <About />
+      <Skill />
+      <Education />
+      <Projects />
+      <Contact />
+    </main>
+    <Footer />
+    
     </>
   )
 }
